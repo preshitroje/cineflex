6f2426da68d2c9ad1ad4f08dc35e57cccbd6d6a6
preshit <preshit2322@gmail.com>
2023-06-18 21:20:13 +0530

    final commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="headerLeft">
+        <Link to="/">
+          <img
+            className="header__icon"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png"
+          />
+        </Link>
+
+        <Link className="headerTxt" to="/">
+          <span>Home</span>
+        </Link>
+
+        <Link className="headerTxt" to="/movies/popular">
+          <span>Popular</span>
+        </Link>
+
+        <Link className="headerTxt" to="/movies/top_rated">
+          <span>Top Rated</span>
+        </Link>
+
+        <Link className="headerTxt" to="/movies/upcoming">
+          <span>Upcoming</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
